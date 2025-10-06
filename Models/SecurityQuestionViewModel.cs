121824402a975d4c0ca62aea266aaa80e1d516bb
Kayla Keesha Williams <s226211983@mandela.ac.za>
2025-10-06 10:16:00 +0200

    fixed the foreget password

---
+++ Models/SecurityQuestionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FridgeManagementSystem.Models
+{
+    public class SecurityQuestionViewModel
+    {
+        public string Email { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+    }
+}
