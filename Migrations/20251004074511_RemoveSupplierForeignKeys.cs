14859c6cc80e9420d1ff5f2d865814d58e427873
Lonia Dipuo Tshokotshela <s227046986@mandela.ac.za>
2025-10-11 19:18:54 +0200

    o

---
+++ Migrations/20251004074511_RemoveSupplierForeignKeys.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FridgeManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSupplierForeignKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseOrderID",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "QuotationID",
+                table: "Suppliers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PurchaseOrderID",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuotationID",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
